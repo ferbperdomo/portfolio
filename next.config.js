ffcb4432a9fb3be37d7893f8bcf2cb18d0e9ad54
Cristian Perdomo <ferperdomo14@gmail.com>
2025-10-23 12:23:42 +0200

    Add next-intl configuration for internationalization

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Optimización de imágenes
@@ -40,4 +44,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
